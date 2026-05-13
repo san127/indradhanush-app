a221f542212ec42884e20fe12b678cff37212aed
sani <saniyabhartu432@gmail.com>
2026-05-14 01:03:54 +0530

    Fix gitignore tracking

--- indradhanush/lib/config/supabase_config.dart
+++
@@ -1,4 +0,0 @@
-class SupabaseConfig {
-  static const String supabaseUrl = 'https://rflwethnybadhactosiu.supabase.co/rest/v1/';
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmbHdldGhueWJhZGhhY3Rvc2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNzI5MDEsImV4cCI6MjA5MzY0ODkwMX0.JpSMHUcK3ByLMWpUP5bOJJ6WxmyX_BkWAnxtTJ_3c8k';
-}
